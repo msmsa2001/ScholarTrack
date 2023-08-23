0033ce4fae3fd6ec0009c4742e136357c8f8aeb5
ahmadkhan5346 <khansarfraz5346@gmail.com>
2023-08-23 17:07:12 +0530

    nt002

--- app/__init__.py
+++ app/__init__.py
@@ -14,6 +14,10 @@ def create_app():
 
 
     from app.auth.controllers import auth_blueprint
+    class multiply:
+        def add(a,b):
+            c=a*b
+            return c
 
     app.register_blueprint(
         auth_blueprint,
